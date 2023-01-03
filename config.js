b59c967962994fbddc33a24d3da32d7ae54bce5f
trongts <trongts@sphinxjsc.com>
2023-01-04 00:04:39 +0700

    admin

---
+++ config.js
@@ -0,0 +1,29 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    
+    texts: [
+        "Chào bạn !!!...",
+         "Hôm nay là sinh nhật của bạn",
+         "Đây là quà của tôi",
+         "Hôm nay là sinh nhật lần thứ N+ của bạn",
+         "Sinh nhật năm ngoái",
+         "Tôi chưa được ăn chùa",
+         "Tôi muốn ăn ngon trong năm nay",
+         "Tôi mong bạn có thể giới thiệu cho tôi vài options",
+         "1 lần nữa",
+         "Sinh nhật vui vẻ nhé :P",
+    ],
+    imgs: {
+        "Hôm nay là sinh nhật của bạn": "./imgs/trang.jpg",
+    },
+    desc: {
+        turn_on: "Bắt đầu",
+        play: "Phát nhạc",
+        bannar_coming: "Lên đèn",
+        balloons_flying: "Hình như còn thiếu thứ gì đó",
+        cake_fadein: "Bánh?",
+        light_candle: "Nến?",
+        wish_message: "Chúc mừng sinh nhật",
+        story: "Happy birth day",
+    }
+};
